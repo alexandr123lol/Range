d71c454bce448ce80ba91e1951147761f1739b5f
Alexander <rffgflfgl.mail.ru>
2016-10-12 21:13:00 +0700

    Range

--- Range1/src/Range/Main.java
+++ Range1/src/Range/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Range range = new Range(2.3, 4.2);
         double number = 3.1;
-        System.out.printf("Числовой диапазон длиной = %.2f  число %.2f принадлежит  ли диапазону: %s",range.getLength(),number,range.inInside(number));
+        System.out.printf("Числовой диапазон длиной = %.2f  число %.2f принадлежит  ли диапазону: %s", range.getLength(), number, range.inInside(number));
 
     }
 }
