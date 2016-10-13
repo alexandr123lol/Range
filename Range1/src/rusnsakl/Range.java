b3e7d10dee7a882976b1523619acbd77432c5f77
Alexander <rffgflfgl.mail.ru>
2016-10-13 23:13:25 +0700

    Range

--- Range1/src/rusnsakl/Range.java
+++ Range1/src/rusnsakl/Range.java
@@ -7,41 +7,35 @@ public class Range {
 
     private double from;
     private double to;
-    private double begin;
-    private double end;
 
-    public Range(double from, double to, double begin, double end) {
+
+    public Range(double from, double to) {
         this.from = from;
         this.to = to;
-        this.begin = begin;
-        this.end = end;
+
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public double getTo() {
+        return to;
     }
 
     public double getLength() {
         return to - from;
     }
 
-    public double getLength2() {
-        return end - begin;
-    }
 
     public boolean isInside(double number) {
         return from <= number && number <= to;
 
     }
 
-    public String  intersectionRange() {
-        from = (begin > from) ? begin : from;
-        to = (end < to) ? end : to;
-
-        if (from > to) {
-            return null;
-        } else {
-            return "["+from+".."+to+"]";
-        }
-    }
-
-
-
 
 }
+
+
+
+
