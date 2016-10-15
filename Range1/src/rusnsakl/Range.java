5a2096a7048ec9427ce7bbd2cd63c66c24a307b3
Alexander <rffgflfgl.mail.ru>
2016-10-16 00:10:11 +0700

    Range

--- Range1/src/rusnsakl/Range.java
+++ Range1/src/rusnsakl/Range.java
@@ -1,5 +1,7 @@
 package rusnsakl;
 
+import java.util.Arrays;
+
 /**
  * Created by Alexander on 13.10.2016.
  */
@@ -45,6 +47,53 @@ public class Range {
         }
         return new Range(begin, end);
 
+    }
+
+    public Range[] association(Range range) {
+        Range[] array = new Range[2];
+
+        if (range.isInside(range.from) && range.isInside(range.to)) {
+            array[0] = new Range(from, to);
+            return array;
+        } else if (range.isInside((range.from))) {
+            array[0] = new Range(from, range.to);
+            return array;
+        } else if (range.isInside(range.to)) {
+            array[0] = new Range(range.from, to);
+            return array;
+        } else if ((range.from <= from && from <= range.to) && (range.from <= to && to <= range.to)) {
+            array[0] = new Range(range.from, range.to);
+            return array;
+
+        } else {
+            if (range.from > from) {
+                array[0] = new Range(from, to);
+                array[1] = new Range(range.from, range.to);
+                return array;
+            } else {
+                array[0] = new Range(range.from, range.to);
+                array[1] = new Range(from, to);
+                return array;
+            }
+        }
+
+    }
+
+    public Range[] difference(Range range) {
+        Range[] array = new Range[2];
+        if (!range.isInside(range.from) && !range.isInside(range.to)) {
+            array[0] = new Range(from, to);
+            return array;
+        } else if (range.isInside(range.from)) {
+            array[0] = new Range(from, range.from);
+            return array;
+        } else if (range.isInside(range.to)) {
+            array[0] = new Range(range.to, to);
+            return array;
+        } else
+            array[0] = new Range(from, range.from);
+        array[1] = new Range(range.to, to);
+        return array;
     }
 }
 
